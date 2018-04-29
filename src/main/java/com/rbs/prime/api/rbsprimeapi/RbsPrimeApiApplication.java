e75d3822b058f8219b44cde4ae03a02d11242a35
dinushaileperuma <38821303+dinushaileperuma@users.noreply.github.com>
2018-04-29 01:34:18 +0100

    Created

---
+++ src/main/java/com/rbs/prime/api/rbsprimeapi/RbsPrimeApiApplication.java
@@ -0,0 +1,12 @@
+package com.rbs.prime.api.rbsprimeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbsPrimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbsPrimeApiApplication.class, args);
+	}
+}
